22d9729e4cdb42e358d065fb45520b7c1defd8c6
Erik Dubois <erik.dubois@gmail.com>
2023-01-21 14:47:08 +0100

    kde

--- configz/alis-config-kde.sh
+++ configz/alis-config-kde.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 set -eu
 
-installed_dir=$(dirname $(readlink -f $(basename `pwd`)))
-
-sed -i "s/DESKTOP_ENVIRONMENT=.*/DESKTOP_ENVIRONMENT=\"kde\"/" $installed_dir/alis.conf
+sed -i "s/DESKTOP_ENVIRONMENT=.*/DESKTOP_ENVIRONMENT=\"kde\"/" /alis/alis.conf
