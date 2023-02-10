2e8674dc654c700d445a676515c647437440bbc2
Erik Dubois <erik.dubois@gmail.com>
2023-02-10 09:10:46 +0100

    typo

--- configz/alis-config-efi-ext4-luks-lvm-systemd-systemdhomed.sh
+++ configz/alis-config-efi-ext4-luks-lvm-systemd-systemdhomed.sh
@@ -6,4 +6,4 @@ sed -i "s/LUKS_PASSWORD=.*/LUKS_PASSWORD=\"erik\"/" /alis/alis.conf
 sed -i "s/LUKS_PASSWORD_RETYPE=.*/LUKS_PASSWORD_RETYPE=\"erik\"/" /alis/alis.conf
 sed -i "s/SYSTEMD_HOMED=.*/SYSTEMD_HOMED=\"true\"/" /alis/alis.conf
 sed -i "s/SYSTEMD_HOMED_STORAGE=.*/SYSTEMD_HOMED_STORAGE=\"luks\"/" /alis/alis.conf
-sed -i "s/SYSTEMD_HOMED_STORAGE_LUKS_TYPE=.*/SYSTEMD_HOMED_STORAGE_LUKS_TYPE=\"ext4\")/" /alis/alis.conf
+sed -i "s/SYSTEMD_HOMED_STORAGE_LUKS_TYPE=.*/SYSTEMD_HOMED_STORAGE_LUKS_TYPE=\"ext4\"/" /alis/alis.conf
